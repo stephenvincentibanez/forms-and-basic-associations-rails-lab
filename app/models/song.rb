ad4a9c7288b057a25cae5f1804453d283cba956b
Stephen Ibanez <sibanez93@Stephens-MacBook-Pro-3.local>
2020-10-31 12:43:25 -0700

    commit tree ff6c50d244e24cfd483245ada444d806f4d44f4f

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,36 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def genre_name
+      self.genre ? self.genre.name : nil 
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def note_contents
+    self.notes.collect(&:content)
+  end
+
+  def note_contents=(note_contents)
+    note_contents.each do |content|
+      unless content.empty?
+        self.notes << Note.create(content: content)
+        self.save 
+      end
+    end
+  end
+
 end
